c915a8e069bf568842522487a3d2ad76d9e52d08
oscar <twocap06@gmail.com>
2020-11-04 15:21:52 +0000

    Loptop

--- Notion_readwise/main.py
+++ Notion_readwise/main.py
@@ -1,15 +1,57 @@
 #!/usr/bin/env python3
 
 from notion.client import NotionClient
+from notion.block import TextBlock
 
 with open("data.txt", "r") as f:
-    token = f.read().splitlines()[0]
-    link = "https://www.notion.so/2606f0ff4bcd47cebc1eed1a4c92581d?v=019319c195e7429a9484c84ad178df77"
+    data = f.read().split("\n")
+    token = data[0]
+    read_later_id = data[1]
+    highlights_id = data[2]
 
 client = NotionClient(token_v2=token)
 
-cv = client.get_collection_view(link)
+read_later = client.get_collection_view(read_later_id)
+highlights = client.get_collection_view(highlights_id)
 
-for row in cv.collection.get_rows():
+#[r.remove() for r in highlights.collection.get_rows()]
+
+highlight_titles = [r.title for r in highlights.collection.get_rows()]
+
+for i, row in enumerate(read_later.collection.get_rows()):
+    if row.title not in highlight_titles:
+        added = highlights.collection.add_row()
+        added.title = row.title
+
+    highlight_blocks = [b.title for b in highlights.collection.get_rows()[i].children]
+
+    new_blocks = []
+
+    for block in row.children:
+        if block.title not in highlight_blocks and block.get(path="format.block_color"):
+            new_blocks.append(block.title)
+
+    page = highlights.collection.get_rows()[i]
+
+    if new_blocks == []:
+        page.remove()
+        continue
+
+    for block in new_blocks:
+        new_block = page.children.add_new(TextBlock, title=block)
+        new_block.set("format.block_color", "yellow_background")
+
+"""
     for block in row.children:
         color = block.get(path="format.block_color")
+        if color:
+            new_blocks.append(str(block.title))
+            new_blocks.append("")
+
+    if new_blocks != []:
+        added = highlights.collection.add_row()
+        added.title = row.title
+
+        [added.children.add_new(TextBlock, title=t) for t in new_blocks]
+"""
+    
