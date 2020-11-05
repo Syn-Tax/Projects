8e0fe73e0b293d933aa9f726dfb0b1f323e1e450
oscar <twocap06@gmail.com>
2020-11-05 11:49:57 +0000

    Loptop

--- Notion_readwise/main.py
+++ Notion_readwise/main.py
@@ -4,20 +4,20 @@ from notion.client import NotionClient
 from notion.block import TextBlock
 import time
 
+update_time = 300
+
 with open("data.txt", "r") as f:
     data = f.read().split("\n")
     token = data[0]
     read_later_id = data[1]
     highlights_id = data[2]
-
+    
 client = NotionClient(token_v2=token)
 
-db = client.get_collection_view(read_later_id)
-
-def update_highlights(record):
-    read_later = record
+while True:
+    read_later = client.get_collection_view(read_later_id)
     highlights = client.get_collection_view(highlights_id)
-
+    
     highlight_titles = [r.title for r in highlights.collection.get_rows()]
 
     for i, row in enumerate(read_later.collection.get_rows()):
@@ -30,28 +30,19 @@ def update_highlights(record):
         new_blocks = []
 
         for block in row.children:
-            if block.title not in highlight_blocks and block.get(path="format.block_color"):
-                new_blocks.append(block.title)
+            if block.type == "text":
+                if block.title not in highlight_blocks and block.get(path="format.block_color"):
+                    new_blocks.append(block.title)
 
         page = highlights.collection.get_rows()[i]
 
         if new_blocks == []:
-            page.remove()
+            page.empty = True
             continue
 
         for block in new_blocks:
             new_block = page.children.add_new(TextBlock, title=block)
-    print("Highlights updated")
+            print("Highlights updated")
 
+    time.sleep(update_time)
 
-current_blocks = []
-
-update_highlights(db)
-
-while True:
-    for row in db.collection.children:
-        if row.title not in current_blocks:
-            row.add_callback(update_highlights)
-            current_blocks.append(row.title)
-
-    time.sleep(300)
