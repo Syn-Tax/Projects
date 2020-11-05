dc3781615215877e318e4c481c2537f5a53ee16b
oscar <twocap06@gmail.com>
2020-11-05 09:15:01 +0000

    Loptop

--- Notion_readwise/main.py
+++ Notion_readwise/main.py
@@ -11,47 +11,34 @@ with open("data.txt", "r") as f:
 
 client = NotionClient(token_v2=token)
 
-read_later = client.get_collection_view(read_later_id)
-highlights = client.get_collection_view(highlights_id)
+db = client.get_collection_view(read_later_id)
 
-#[r.remove() for r in highlights.collection.get_rows()]
+def update_highlights(record):
+    read_later = record
+    highlights = client.get_collection_view(highlights_id)
 
-highlight_titles = [r.title for r in highlights.collection.get_rows()]
+    highlight_titles = [r.title for r in highlights.collection.get_rows()]
 
-for i, row in enumerate(read_later.collection.get_rows()):
-    if row.title not in highlight_titles:
-        added = highlights.collection.add_row()
-        added.title = row.title
+    for i, row in enumerate(read_later.collection.get_rows()):
+        if row.title not in highlight_titles:
+            added = highlights.collection.add_row()
+            added.title = row.title
 
-    highlight_blocks = [b.title for b in highlights.collection.get_rows()[i].children]
+        highlight_blocks = [b.title for b in highlights.collection.get_rows()[i].children]
 
-    new_blocks = []
+        new_blocks = []
 
-    for block in row.children:
-        if block.title not in highlight_blocks and block.get(path="format.block_color"):
-            new_blocks.append(block.title)
+        for block in row.children:
+            if block.title not in highlight_blocks and block.get(path="format.block_color"):
+                new_blocks.append(block.title)
 
-    page = highlights.collection.get_rows()[i]
+        page = highlights.collection.get_rows()[i]
 
-    if new_blocks == []:
-        page.remove()
-        continue
+        if new_blocks == []:
+            page.remove()
+            continue
 
-    for block in new_blocks:
-        new_block = page.children.add_new(TextBlock, title=block)
-        new_block.set("format.block_color", "yellow_background")
+        for block in new_blocks:
+            new_block = page.children.add_new(TextBlock, title=block)
 
-"""
-    for block in row.children:
-        color = block.get(path="format.block_color")
-        if color:
-            new_blocks.append(str(block.title))
-            new_blocks.append("")
-
-    if new_blocks != []:
-        added = highlights.collection.add_row()
-        added.title = row.title
-
-        [added.children.add_new(TextBlock, title=t) for t in new_blocks]
-"""
-    
+db.add_callback(update_highlights)
