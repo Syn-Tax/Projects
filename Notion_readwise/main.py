1c4ca2009553fb9809d9ee5a11d046dda2307e8f
Oscar Morris <twocap06@gmail.com>
2020-11-07 20:33:00 +0000

    something

--- Notion_readwise/main.py
+++ Notion_readwise/main.py
@@ -3,6 +3,9 @@
 from notion.client import NotionClient
 from notion.block import TextBlock
 import time
+import re
+
+update_time = 180
 
 with open("data.txt", "r") as f:
     data = f.read().split("\n")
@@ -15,24 +18,38 @@ client = NotionClient(token_v2=token)
 read_later = client.get_collection_view(read_later_id)
 highlights = client.get_collection_view(highlights_id)
 
+def clean_text(text):
+    output = re.sub(r"\[(.*?)\]\(.*?\)", r"\1", text)
+    output = output.rstrip()
+    return output
+
 def update_highlights():
-    highlight_titles = [r.title for r in highlights.collection.get_rows()]
+    highlight_rows = highlights.collection.get_rows()
+
+    highlight_titles = [r.title for r in highlight_rows]
 
     for i, row in enumerate(read_later.collection.get_rows()):
+        print("----------------------------------------------------")
         if row.title not in highlight_titles:
             added = highlights.collection.add_row()
             added.title = row.title
+            highlight_rows = highlights.collection.get_rows()
+            print("{}\n{}".format(added.title, highlight_rows[i].title))
 
-        highlight_blocks = [b.title for b in highlights.collection.get_rows()[i].children]
+        highlight_blocks = [clean_text(b.title) for b in highlight_rows[i].children]
 
         new_blocks = []
 
         for block in row.children:
             if block.type == "text":
-                if block.title not in highlight_blocks and block.get(path="format.block_color"):
-                    new_blocks.append(block.title)
+                title = clean_text(block.title)
+                if title not in highlight_blocks and block.get(path="format.block_color"):
+                    new_blocks.append(title)
 
-        page = highlights.collection.get_rows()[i]
+        print(highlight_blocks)
+        print(new_blocks)
+
+        page = highlight_rows[i]
 
         if highlight_blocks == [] and new_blocks == []:
             page.empty = True
@@ -40,6 +57,9 @@ def update_highlights():
         for block in new_blocks:
             new_block = page.children.add_new(TextBlock, title=block)
             page.empty = False
-            print("Highlights updated")
+        print("Highlights updated")
 
 update_highlights()
+#while True:
+#    update_highlights()
+#    time.sleep(update_time)
